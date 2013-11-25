ba4f948bae9d8569113ed4df21f742815268ef97
Michał '2M1R' Różański <michal.rozanski@gmail.com>
2013-11-25 23:43:20 +0100

    Added some rendering classes

--- pong.py
+++ pong.py
@@ -1,12 +1,30 @@
 import sys
 
 try:
+    from sdl2 import *
     import sdl2.ext as sdl2ext
 except ImportError:
     import traceback
 
     traceback.print_exc()
     sys.exit(1)
+
+WHITE = sdl2ext.Color(255, 255, 255)
+
+
+class SoftwareRenderer(sdl2ext.SoftwareSpriteRenderer):
+    def __init__(self, window):
+        super(SoftwareRenderer, self).__init__(window)
+
+    def render(self, components):
+        sdl2ext.fill(self.surface, sdl2ext.Color(0, 0, 0))
+        super(SoftwareRenderer, self).render(components)
+
+
+class Player(sdl2ext.Entity):
+    def __init__(self, world, sprite, posx=0, posy=0):
+        self.sprite = sprite
+        self.sprite.position = posx, posy
 
 
 def run():
