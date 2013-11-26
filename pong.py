d60b64a696a12af4df861fd1bb2e34ec952a9fd5
Michał '2M1R' Różański <michal.rozanski@gmail.com>
2013-11-26 15:51:56 +0100

    Ball and movement system

--- pong.py
+++ pong.py
@@ -11,7 +11,6 @@ except ImportError:
 
 WHITE = sdl2ext.Color(255, 255, 255)
 
-
 class SoftwareRenderer(sdl2ext.SoftwareSpriteRenderer):
     def __init__(self, window):
         super(SoftwareRenderer, self).__init__(window)
@@ -20,11 +19,51 @@ class SoftwareRenderer(sdl2ext.SoftwareSpriteRenderer):
         sdl2ext.fill(self.surface, sdl2ext.Color(0, 0, 0))
         super(SoftwareRenderer, self).render(components)
 
-
 class Player(sdl2ext.Entity):
     def __init__(self, world, sprite, posx=0, posy=0):
         self.sprite = sprite
         self.sprite.position = posx, posy
+        self.velocity = Velocity()
+
+
+class MovementSystem(sdl2ext.Applicator):
+    def __init__(self, minx, miny, maxx, maxy):
+        super(MovementSystem, self).__init__()
+        self.componenttypes = (Velocity, sdl2ext.Sprite)
+        self.minx = minx
+        self.miny = miny
+        self.maxx = maxx
+        self.maxy = maxy
+
+    def process(self, world, componentsets):
+        for velocity, sprite in componentsets:
+            swidth, sheight = sprite.size
+            sprite.x += velocity.vx
+            sprite.y += velocity.vy
+
+            sprite.x = max(self.minx, sprite.x)
+            sprite.y = max(self.miny, sprite.y)
+
+            pmaxx = sprite.x + swidth
+            pmaxy = sprite.y + sheight
+            if pmaxx > self.maxx:
+                sprite.x = self.maxx - swidth
+            if pmaxy > self.maxy:
+                sprite.y = self.maxy - sheight
+
+
+class Velocity(object):
+    def __init__(self):
+        super(Velocity, self).__init__()
+        self.vx = 0
+        self.vy = 0
+
+
+class Ball(sdl2ext.Entity):
+    def __init__(self, world, sprite, posx=0, posy=0):
+        self.sprite = sprite
+        self.sprite.position = posx, posy
+        self.velocity = Velocity()
 
 
 def run():
@@ -34,15 +73,20 @@ def run():
 
     world = sdl2ext.World()
 
+    movement = MovementSystem(0, 0, 800, 600)
     spriterenderer = SoftwareRenderer(window)
+    world.add_system(movement)
     world.add_system(spriterenderer)
 
     factory = sdl2ext.SpriteFactory(sdl2ext.SOFTWARE)
     sp_paddle1 = factory.from_color(WHITE, size=(20, 100))
     sp_paddle2 = factory.from_color(WHITE, size=(20, 100))
+    sp_ball = factory.from_color(WHITE, size=(20, 20))
 
     player1 = Player(world, sp_paddle1, 0, 250)
     player2 = Player(world, sp_paddle2, 1255, 250)
+    ball = Ball(world, sp_ball, 390, 290)
+    ball.velocity.vx = -3
 
     running = True
     while running:
